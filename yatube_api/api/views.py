006c4ad4831dc6358e7be44312197c84bcd9b1ef
Даниэль Жариков <dan.zharikow2017@yandex.ru>
2023-03-04 21:02:32 +0200

    Sprint 8. Final project. V2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import get_object_or_404
-from posts.models import Comment, Group, Post, User
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from django.core.exceptions import PermissionDenied
+
+from posts.models import Group, Post, User
 
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (CommentSerializer, GroupSerializer, PostSerializer,
@@ -31,18 +31,6 @@ class PostViewSet(viewsets.ModelViewSet):
         """Create new post with author."""
         serializer.save(author=self.request.user)
 
-    def perform_update(self, serializer):
-        """Update post with author."""
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('You can not update this post')
-        super(PostViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        """Delete post with author."""
-        if instance.author != self.request.user:
-            raise PermissionDenied('You can not delete this post')
-        super(PostViewSet, self).perform_destroy(instance)
-
 
 class CommentViewSet(viewsets.ModelViewSet):
     """Comment model view set."""
@@ -52,21 +40,9 @@ class CommentViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Get comments for post."""
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
-        return Comment.objects.filter(post=post)
+        return post.comments.all()
 
     def perform_create(self, serializer):
         """Create new comment with author."""
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
         serializer.save(author=self.request.user, post=post)
-
-    def perform_update(self, serializer):
-        """Update comment with author."""
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('You can not update this comment')
-        super(CommentViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        """Delete comment with author."""
-        if instance.author != self.request.user:
-            raise PermissionDenied('You can not delete this comment')
-        super(CommentViewSet, self).perform_destroy(instance)
